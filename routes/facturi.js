201f90c692db9f9db4754ae205f96e2a3cc5eaca
Doru Chiulan <doru.chiulan@gmail.com>
2016-02-01 01:14:18 +0200

    factura to pdf

--- routes/facturi.js
+++ routes/facturi.js
@@ -1,19 +1,36 @@
 var express = require('express');
+var path = require('path');
 var router = express.Router();
 var mongoose = require('mongoose');
-var Asset = mongoose.model('Facturi');
+var Factura = mongoose.model('Facturi');
 var multer  = require('multer');
 var upload = multer({ dest: './uploads/' });
+var phantom = require('phantom');
 
 router.get('/', function(req, res, next) {
-  Asset.find().exec(function(err, assets){
+  Factura.find().exec(function(err, assets){
     if(err){ return next(err); }
 
     res.json(assets);
   });
 });
+
+router.get('/download/:id', function (req, res) {
+    phantom.create(function (ph) {
+        ph.createPage(function (page) {
+            page.open("views/index.html", function (status) {
+                console.log("opened google? ", status);
+                var cale = path.join(__dirname, '/github.png');
+                page.render('github.pdf');
+                console.log(cale);
+                res.sendFile(path.join(__dirname, '/../github.pdf'));
+            });
+        });
+    });
+});
+
 router.post('/', function(req, res, next) {
-  var asset = new Asset(req.body);
+  var asset = new Factura(req.body);
   asset.save(function(err, ass){
     if(err){ console.log(err);return next(err); }
 
@@ -21,9 +38,9 @@ router.post('/', function(req, res, next) {
   });
 });
 router.post('/update/', function(req, res, next) {
-  var asset = new Asset(req.body);
+  var asset = new Factura(req.body);
   delete req.body["_id"];
-  Asset.findOneAndUpdate({_id: asset._id}, req.body, {upsert: true}, function(err, asset) {
+  Factura.findOneAndUpdate({_id: asset._id}, req.body, {upsert: true}, function(err, asset) {
     console.log(err);
 
     res.json(asset);
