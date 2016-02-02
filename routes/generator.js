daa6fc2c3e6eb877705e1f8d0390b9a3cac348ed
Andrei Samciuc <andrei@sandd.ro>
2016-02-02 12:28:29 +0200

    automated push

--- routes/generator.js
+++ routes/generator.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 
-router.get('/:id', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.render('bill', { title: 'Factura SandD Soft Works' });
 });
 
