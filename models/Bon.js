822764b41cf04bea5dde77c685f88a7367067614
Doru Chiulan <doru.chiulan@gmail.com>
2016-02-29 02:23:00 +0200

    Fixed model property name.

--- models/Bon.js
+++ models/Bon.js
@@ -1,7 +1,7 @@
 var generate_model = require('./_model.js');
 generate_model("Bon", {
-        produs: String,
-        data: Date,
-        price: Number
+        products: String,
+        date: Date,
+        amount: Number
     }
 );
