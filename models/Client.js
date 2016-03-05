bb195b0654fd05835858dec1a5b35450b1d66248
Doru Chiulan <doru.chiulan@gmail.com>
2016-03-06 01:28:05 +0200

    Fixed client model

--- models/Client.js
+++ models/Client.js
@@ -4,17 +4,13 @@ generate_model("Client", {
         nume: String,
         cif: String,
         orc: String,
-        adresa: {
-            sediu: String,
-            sediu2: String,
-            oras: String,
-            judet: String,
-            tara: String
-        },
+        sediu: String,
+        oras: String,
+        judet: String,
+        tara: String,
         banca: [{
             nume: String,
-            cont: String,
-            valuta: String
+            cont: String
         }]
     }
 );
