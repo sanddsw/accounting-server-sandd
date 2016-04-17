a34be07b43b0855c46e8c37e6424bf8746267da6
Andrei Samciuc <andrei@sandd.ro>
2016-04-17 21:06:10 +0300

    changed

--- models/User.js
+++ models/User.js
@@ -1,12 +1,27 @@
 var generate_model = require('./_model.js');
+var mongoose = require('mongoose');
+var ObjectId = mongoose.Schema.ObjectId;
 
-generate_model("User", {
-    nm: String,
-    ae: String,
-    nr: String,
-    un: String,
-    pw: String,
-    rl: Number,
-    ass: [require('mongoose').Schema.Types.ObjectId]
-  }
+generate_model("User",
+
+    {
+        nume: String,
+        email: String,
+        username: String,
+        roles: [{type: ObjectId, ref: "UserRole"}],
+        password: String
+    }
+    
+    
+    
 );
+
+generate_model('UserRole', {
+    company: {type: ObjectId, ref: "Company"},
+    user: {type: ObjectId, ref: "User"},
+    privileges: [{
+        name: String,
+        read: Boolean,
+        write: Boolean
+    }]
+});
--- routes/users.js
+++ routes/users.js
@@ -3,29 +3,33 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var User = mongoose.model('User');
 
-router.post('/register/', function(req, res, next) {
-    var user = new User(req.body);
-    user.rl = 2;
-    user.save(function(err, us){
-        if(err){ console.log(err);return next(err); }
-
-        res.json({
-            success: true,
-            user: us
-        });
-    });
-});
+// router.post('/register/', function(req, res, next) {
+//     var user = new User(req.body);
+//     user.rl = 2;
+//     user.save(function(err, us){
+//         if(err){ console.log(err);return next(err); }
+//
+//         res.json({
+//             success: true,
+//             user: us
+//         });
+//     });
+// });
 router.post('/login/', function(req, response, next) {
     var loginDetails = req.body;
     var res = {};
-    User.findOne({ un: loginDetails.un }).exec(function (err, user) {
+    var type = (require('email-validator').validate(loginDetails.username)) ? "email" : "username";
+    var query = {};
+    query[type] = loginDetails[type];
+    User.findOne(query).exec(function (err, user) {
         if (err) return handleError(err);
         if(user == null) {
             res = {
                 success: false,
-                reason: "Nu exista un asemenea user!"
+                reason: "Nu exista un asemenea " + type + "!"
             }
-        } else if(user.pw === loginDetails.pw) {
+        } else if(user.password === loginDetails.password) {
+            user.password = "";
             res = {
                 success: true,
                 user: user
@@ -36,6 +40,7 @@ router.post('/login/', function(req, response, next) {
                 reason: "Parola incorecta!"
             }
         }
+        console.log(res);
         response.json(res);
     });
 });
